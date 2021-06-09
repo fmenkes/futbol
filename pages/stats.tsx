34bb7f423b47efe13f9c041b47659732de4f7ab0
Gustav Westberg <gustavwes@gmail.com>
2021-06-10 01:20:32 +0200

    missing files?

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,19 +1,53 @@
-// import React from 'react';
-import { NextPage } from 'next';
+import superjson from 'superjson';
+import { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
+import { Prisma } from '@prisma/client';
+import prisma from '@lib/prisma';
 
 const StatChart = dynamic(() => import('../components/StatChart'))
 
-const Stats: NextPage = () => {
+const userScores = Prisma.validator<Prisma.UserArgs>()({
+    select: { scores: true, name: true },
+  });
+  
+  export type UserScores = Prisma.UserGetPayload<typeof userScores>;
+  
+  type Props = {
+    scores: UserScores[];
+  };
+
+const Stats: NextPage<Props> = ({ scores }) => {
+    console.log(scores);
     return (
         <>
             <Head>
                 <title>Stats</title>
             </Head>
-            <StatChart />
+            <StatChart scores={scores} />
         </>
     );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const scores = await prisma.user.findMany({
+      select: {
+        name: true,
+        scores: true,
+        email: true,
+      },
+    });
+  
+    const scoresWithNames = scores.map((user) => ({
+      ...user,
+      name: user.name || user.email.split('@')[0],
+    }));
+  
+    return {
+      props: {
+        scores: superjson.serialize(scoresWithNames).json,
+      },
+    };
+  };
 
 export default Stats;
